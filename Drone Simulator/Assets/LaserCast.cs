a723249901830d8e296e6e42857adb4504cea665
michaeltangelo <tanman24689@gmail.com>
2016-05-13 08:04:22 -0400

    mode switching, missile firing, targetting complete

---
+++ Drone Simulator/Assets/LaserCast.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class LaserCast : MonoBehaviour {
+    public Camera cam;
+    private Transform lockPoint;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void LaserLock()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out hit))
+        {
+            lockPoint = hit.transform;
+
+            // Do something with object that was hit by raycast
+        }
+    }
+}
--- Drone Simulator/Assets/Standard Assets/Vehicles/Aircraft/Scripts/AeroplaneUserControl4Axis.cs
+++ Drone Simulator/Assets/Standard Assets/Vehicles/Aircraft/Scripts/AeroplaneUserControl4Axis.cs
@@ -4,7 +4,8 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Aeroplane
 {
-	[RequireComponent(typeof (AeroplaneController))]
+    [RequireComponent(typeof(LineRenderer))]
+    [RequireComponent(typeof (AeroplaneController))]
 	public class AeroplaneUserControl4Axis : MonoBehaviour
 	{
 		// these max angles are only used on mobile, due to the way pitch and roll input are handled
@@ -24,8 +25,32 @@ namespace UnityStandardAssets.Vehicles.Aeroplane
 		private bool m_AirBrakes;
 		private float m_Yaw;
 		private bool m_ToggleOperatorMode;
+        private bool m_ToggleLock;
+        private bool squarePressed;
 
-		public void setPilotMode(bool boolean)
+        // control room references
+
+        // reference to the underbelly camera and speed settings
+        public GameObject CameraBay;
+        public float rotateSpeedScale;
+        private Camera cam;
+        public Vector3 lockPoint;
+        private bool isLocked;
+        LineRenderer line;
+        public Material lineMat;
+        public Vector3 cameraPosition;
+        public GameObject Sphere;
+        public Material[] materials;
+
+        // missile properties
+        public Rigidbody Missile;
+        public Transform Launcher;
+        public GameObject Target;
+
+        float roll;
+        float pitch;
+
+        public void setPilotMode(bool boolean)
 		{
 			pilotMode = boolean;
 		}
@@ -39,52 +64,138 @@ namespace UnityStandardAssets.Vehicles.Aeroplane
 		{
 			pilotMode = !pilotMode;
 		}
+        
+        public void toggleLockMode()
+        {
+            Debug.Log("ToggleLockMode called");
+            isLocked = !isLocked;
+        }
 
 		private void Awake()
 		{
 			// Set up the reference to the aeroplane controller.
 			m_Aeroplane = GetComponent<AeroplaneController>();
+            CameraBay = GameObject.Find("UnderbellyCam");
+            cam = CameraBay.GetComponent<Camera>();
+            isLocked = false;
+            line = GetComponent<LineRenderer>();
+            line.SetVertexCount(2);
+            line.GetComponent<Renderer>().material= lineMat;
+            line.SetWidth(0.01f, 0.05f);
+
 		}
 
+        private void LockLaser()
+        {
+            GameObject go = GameObject.FindGameObjectWithTag("target");
+            Destroy(go);
+            RaycastHit hit;
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            if (Physics.Raycast(ray, out hit))
+            {
+                isLocked = true;
+                lockPoint = hit.point;
+                GameObject TargetCopy = Instantiate(Target, hit.point, Quaternion.identity) as GameObject;
+                TargetCopy.tag = "target";
+                if (hit.collider.tag == "sphere")
+                {
+                    Sphere.GetComponent<Renderer>().material = materials[1];
+                }
+            }
+            
+        }
+
+        private void Update()
+        {
+            // Read input for the pitch, yaw, roll and throttle of the aeroplane.
+            if (!controllerEnabled)
+            {
+                roll = CrossPlatformInputManager.GetAxis("Horizontal");
+                pitch = CrossPlatformInputManager.GetAxis("Vertical");
+                m_AirBrakes = CrossPlatformInputManager.GetButton("Fire1");
+                m_Yaw = CrossPlatformInputManager.GetAxis("Horizontal");
+                m_Throttle = CrossPlatformInputManager.GetAxis("Throttle_Vertical");
+            }
+            else
+            {
+                // ps4 controller map https://www.reddit.com/r/Unity3D/comments/1syswe/ps4_controller_map_for_unit/
+                roll = CrossPlatformInputManager.GetAxis("PS3ControllerRightX");
+                pitch = CrossPlatformInputManager.GetAxis("PS3ControllerRightY");
+                m_AirBrakes = CrossPlatformInputManager.GetButtonDown("Fire1"); // Joystick 4 aka L1 on PS4 Controller
+                m_Yaw = CrossPlatformInputManager.GetAxis("PS3ControllerRightX");
+                m_Throttle = CrossPlatformInputManager.GetAxis("PS3ControllerThrottleY");
+                m_ToggleOperatorMode = CrossPlatformInputManager.GetButtonDown("PS3ControllerOButton");
+                m_ToggleLock = CrossPlatformInputManager.GetButtonDown("PS3ControllerLock");
+                squarePressed = CrossPlatformInputManager.GetButton("PS3ControllerSquare");
+            }
+            if (squarePressed)
+            {
+                UnityEngine.VR.InputTracking.Recenter();
+            }
+            if (m_ToggleOperatorMode) togglePilotMode();
+            if (m_AirBrakes)
+            {
+                Rigidbody missileClone = (Rigidbody)Instantiate(Missile, Launcher.transform.position, Launcher.transform.rotation);
+            }
+            if (m_ToggleLock)
+            {
+                toggleLockMode();
+                if (isLocked) LockLaser();
+                else
+                {
+                    Sphere.GetComponent<Renderer>().material = materials[0];
+                }
+            }
+        }
 
 		private void FixedUpdate()
 		{
-			float roll;
-			float pitch;
-			// Read input for the pitch, yaw, roll and throttle of the aeroplane.
-			if (!controllerEnabled)
-			{
-				roll = CrossPlatformInputManager.GetAxis("Horizontal");
-				pitch = CrossPlatformInputManager.GetAxis("Vertical");
-				m_AirBrakes = CrossPlatformInputManager.GetButton("Fire1");
-				m_Yaw = CrossPlatformInputManager.GetAxis("Horizontal");
-				m_Throttle = CrossPlatformInputManager.GetAxis("Throttle_Vertical");
-			}
-			else
-			{
-				// ps4 controller map https://www.reddit.com/r/Unity3D/comments/1syswe/ps4_controller_map_for_unit/y
-				roll = CrossPlatformInputManager.GetAxis("PS3ControllerRightX");
-				pitch = CrossPlatformInputManager.GetAxis("PS3ControllerRightY");
-				m_AirBrakes = CrossPlatformInputManager.GetButton("Fire1"); // Joystick 4 aka L1 on PS4 Controller
-				m_Yaw = CrossPlatformInputManager.GetAxis("PS3ControllerRightX");
-				m_Throttle = CrossPlatformInputManager.GetAxis("PS3ControllerThrottleY");
-				m_ToggleOperatorMode = CrossPlatformInputManager.GetButtonDown("PS3ControllerOButton");
-			}
 			#if MOBILE_INPUT
 			AdjustInputForMobileControls(ref roll, ref pitch, ref m_Throttle);
 			#endif
 
-			if (m_ToggleOperatorMode) togglePilotMode();
+            // Pass the input to the aeroplane
+            // if pilot mode disabled, send no signals to plane except throttle and turn camera instead
+            if (pilotMode)
+            {
+                m_Aeroplane.Move(roll, pitch, m_Yaw, m_Throttle, m_AirBrakes);
+                line.enabled = false;
+            }
+            else
+            // in sensor operator mode
+            {
+                /*
+                cameraPosition = cam.transform.position;
+                RaycastHit hit;
+                Vector3 rayStartOffset = cam.transform.position;
+                Ray ray = new Ray(rayStartOffset, cam.transform.forward);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    line.enabled = true;
+                    line.SetPosition(0, cam.transform.position);
+                    line.SetPosition(1, hit.point);
+                    //Debug.Log("Hit!");
+                }
+                else
+                {
+                    line.enabled = false;
+                }
+                */
+                // move airplane
+                m_Aeroplane.Move(0, 0, 0, m_Throttle, m_AirBrakes);
 
-			// Pass the input to the aeroplane
-			// if pilot mode disabled, send no signals to plane except throttle and turn camera instead
-			if (pilotMode)
-			{
-				m_Aeroplane.Move(roll, pitch, m_Yaw, m_Throttle, m_AirBrakes);
-			}
-			else
-			{
-				m_Aeroplane.Move(0, 0, 0, m_Throttle, m_AirBrakes);
+                // lock camera to target
+                if (isLocked)
+                {
+                    CameraBay.transform.LookAt(lockPoint);
+                }
+                else
+                {
+                    // rotates right
+                    CameraBay.transform.Rotate(Vector3.up * rotateSpeedScale * Time.deltaTime * roll, Space.Self);
+                    // rotates down
+                    CameraBay.transform.Rotate(Vector3.left * rotateSpeedScale * Time.deltaTime * pitch);
+                }
 			}
 		}
 
